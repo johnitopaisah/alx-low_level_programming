ce0392b8f643e960471a0950df863d5fcb790afb
johnitopaisah <johnitopaisah@gmail.com>
2023-03-22 23:29:31 +0000

    Function that prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - Function that prints the sign of a numbeer.
+ * @n: parameter for the function
  *
  * Return: Return 1 if positive, -1 if negative and 0 otherwise
  */
@@ -12,7 +13,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	 else if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
