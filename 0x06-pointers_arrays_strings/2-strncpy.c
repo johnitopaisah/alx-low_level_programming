589624907ecb09f45daa1c50ff5dc9e4a8d27c6f
johnitopaisah <johnitopaisah@gmail.com>
2023-03-30 11:03:31 +0000

    function that concatenate n string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,19 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
+	int src_len = 0;
 
+	while (src[src_len++])
+	{
+		src_len++;
+	}
 	for (index = 0; src[index] && index < n; index++)
 	{
 		dest[index] = src[index];
+	}
+	for (index = src_len; src[src_len] && index < n; index++)
+	{
+		src[src_len] = '\0';
 	}
 	return (dest);
 }
