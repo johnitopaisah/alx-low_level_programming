25c371f68b58f48f241b42b17a7bad0b76c61d2a
johnitopaisah <johnitopaisah@gmail.com>
2023-04-09 19:59:11 +0100

    function that add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,36 @@
 #include <stdio.h>
 
 /**
+ * _atoi - function that convert strings to integer
+ * @s: the string to be converted
+ *
+ * Return: Return the integer converted
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = ((num * 10) + (s[i] - '0'));
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+	}
+	return (num * sign);
+}
+
+/**
  * main - function that adds positive numbers
  * @argc: counter of the arguments passed
  * @argv: pointer to the arguments vector passed
@@ -16,17 +46,17 @@ int main(int argc, char *argv[])
 	{
 		printf("0\n");
 	}
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (j = i; argv[j]; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-			if (!(argv[i][j] > '0' && argv[i][j] <= '9'))
+			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
 			{
 				printf("Error\n");
 				return (1);
 			}
-		sum += *argv[i];
 		}
+		sum += _atoi(argv[i]);
 	}
 	printf("sum is: %d\n", sum);
 	return (0);
