3604db759bf36c61c70814eaaeaec36f99e9cfe3
johnitopaisah <johnitopaisah@gmail.com>
2023-04-10 23:25:40 +0100

    function that locates a substring

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: pointer to the string
+ * @needle: pointer to the  substing
+ * Return: pointet to the string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
+	return (haystack);
+}
