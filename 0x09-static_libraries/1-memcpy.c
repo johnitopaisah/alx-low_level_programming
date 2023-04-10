d41cd0129a2c188480ee26cb70ee8956c1476c8b
johnitopaisah <johnitopaisah@gmail.com>
2023-04-10 23:01:18 +0100

    function that fill memory area with constant bytes

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy(char *dest, char *src, unsigned int n)
+ * @dest: pointer to the destination memory location to copy to
+ * @src: pointer to the source memory location to copy from
+ * @n: number of byte to copy
+ *
+ * Return: pointer to the the dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int index;
+	
+	for (index = 0; index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+	return (dest);
+}
