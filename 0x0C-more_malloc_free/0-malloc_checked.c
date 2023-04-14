0052c5bee1e006f7c1aa14b24d7b8b8796120ffe
johnitopaisah <johnitopaisah@gmail.com>
2023-04-14 23:10:31 +0100

    function that allocate memory location using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: size of memory block to be allocated
+ *
+ * Return: pointer to the allocated memory
+ * terminates with value of 98 if malloac fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pt;
+
+	pt = malloc(b);
+	if (pt == NULL)
+	{
+		exit(98);
+	}
+	return (98);
+}
