102b621f90820fef48d0ec976850564b8a1b535e
johnitopaisah <johnitopaisah@gmail.com>
2023-04-17 22:23:15 +0100

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - short description
+ * @name: first member of the struct
+ * @age: second member of the struct
+ * @owner: third member of the struct
+ * @Description: This struct data type take three members
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
