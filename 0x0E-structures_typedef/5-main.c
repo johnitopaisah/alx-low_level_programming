3a716b1c168cee0b7617b918c63170b69021fd66
johnitopaisah <johnitopaisah@gmail.com>
2023-04-18 15:22:18 +0100

    function that frees dogs

--- 0x0E-structures_typedef/5-main.c
+++ 0x0E-structures_typedef/5-main.c
@@ -10,8 +10,8 @@ int main(void)
 {
 	dog_t *my_dog;
 
-	my_dog = my_dog("Poppy", 3.5, "Bob");
-	print("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+	my_dog = new_dog("Poppy", 3.5, "Bob");
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
 	free_dog(my_dog);
 	return (0);
 }
