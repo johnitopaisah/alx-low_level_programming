a46f9f378f51bdbb821c792621801e5997e251c0
johnitopaisah <johnitopaisah@gmail.com>
2023-04-19 04:42:52 +0100

    Score: 0.0% (Checks completed: 0.0%)

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro_h
+ * Desc: function_like macro that computes the absolute value of x
+ *
+ */
+
+#define ABS(x)  ((x) < 0 ? (-x) : (x))
+
+#endif
