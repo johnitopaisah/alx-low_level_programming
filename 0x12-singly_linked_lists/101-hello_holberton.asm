6f7908e2f493b590170e6df2072112036dbd4ec4
johnitopaisah <johnitopaisah@gmail.com>
2023-04-29 04:38:21 +0100

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,33 @@
-section .data
-	msg db 'Hello, Holberton', 0Ah ; Message to print
+; Comments: My first hand on assecly language
+; File: 101-hello)holberton.asm
+; Author: John I. Isah
+; Description; A 64-bit assembly program that prints Hello,
+;		Holberton and its followed by a new line.
+
+extern printf
 
 section .text
 	global main
 
 main:
 	push rbp	; Setup stack frame
-	mov rbp, rsp
-	mov rdi, msg	; Load address of message into rdi
-	xor eax, eax 	; Clear eaz to indicate suxxess
-	call printf	; Call the C printf function
-	mov rsp, rbp	; Restore stack pointer
-	pop rbp		; Tear down stack frame
-	ret		; Return to the operating system
+
+	mov rdi, fmt
+	mov rsi, msg
+	mov rax, 0
+	call printf
+
+	pop rbp
+
+	mov rax, 0
+	ret
+
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
+
+;	xor eax, eax 	; Clear eaz to indicate suxxess
+;	call printf	; Call the C printf function
+;	mov rsp, rbp	; Restore stack pointer
+;	pop rbp		; Tear down stack frame
+;	ret		; Return to the operating system
