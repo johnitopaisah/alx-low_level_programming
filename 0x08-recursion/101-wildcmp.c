beef8c662ef6dc340787774228d80ca3250576e4
johnitopaisah <johnitopaisah@gmail.com>
2023-05-01 08:29:49 +0100

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,21 +12,23 @@ int wildcmp(char *strg1, char *strg2)
 {
 	if (*strg2 == '*')
 	{
-		if (*strg1 == '\0' && *(strg2 + 1) == '\0')
-			return (1);
-		if  (*strg1 == '\0' || *(strg2 + 1) == '\0')
-			return (0);
-		else if (wildcmp(strg1, strg2 + 1) || wildcmp(strg1 + 1, strg2))
-			return (1);
+		if (*(strg2 + 1) == '*')
+			return (wildcmp(strg1, strg2 + 1));
+		else if (!*strg1)
+			return (wildcmp(strg1, strg2 + 1));
+		else
+			return (wildcmp(strg1, strg2 + 1) || wildcmp(strg1 + 1, strg2));
 	}
-	else if (*strg1 != '\0' && *strg2 != '\0' && (*strg1 == *strg2 || *strg2 == '?'))
+	else
 	{
-		if (wildcmp(strg1 + 1, strg2 + 1))
-			return (1);
+		if (*strg1 == *strg2)
+		{
+			if (!*strg1)
+				return (1);
+			else
+				return (wildcmp(strg1 + 1, strg2 + 1));
+		}
 	}
-	else if (*strg1 == '\0' && *strg2 == '\0')
-		return (1);
 
 	return (0);
-
 }
