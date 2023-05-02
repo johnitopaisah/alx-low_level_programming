4e22a1eaf649876c21833c6441ce149a099488cb
johnitopaisah <johnitopaisah@gmail.com>
2023-05-02 22:33:26 +0100

    function that inserts a new node at a given position.

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_nodeint_at_index - Inserts a new node to a listint_t
  * list at a given position.
@@ -14,11 +15,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *copy = *head;
 	unsigned int node;
-	
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->n = n;
 	if (idx == 0)
 	{
@@ -26,14 +27,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (new);
 	}
-	
+
 	for (node = 0; node < (idx - 1); node++)
 	{
 		if (copy == NULL || copy->next == NULL)
 			return (NULL);
 		copy = copy->next;
 	}
-	
+
 	new->next = copy->next;
 	copy->next = new;
 	return (new);
