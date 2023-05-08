d9dc25816ecac959ddfca361e028debf71a6e0b5
johnitopaisah <johnitopaisah@gmail.com>
2023-05-09 00:43:10 +0100

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
 
 /* all the functions prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
