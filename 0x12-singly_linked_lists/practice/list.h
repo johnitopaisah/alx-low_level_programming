cb834cd7073b9c6ff4078deddb61ab2235796623
johnitopaisah <johnitopaisah@gmail.com>
2023-07-04 20:59:09 +0100

    header file

---
+++ 0x12-singly_linked_lists/practice/list.h
@@ -0,0 +1,21 @@
+#ifndef LIST_H
+#define LIST_H
+
+/**
+ * struc list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: pointer to the next node
+ *
+ * Description: singly linked node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /* LIST_H */
