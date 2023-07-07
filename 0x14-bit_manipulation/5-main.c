102040d7babae1c5788a87175086a143f976ef14
johnitopaisah <johnitopaisah@gmail.com>
2023-07-07 22:39:17 +0100

    function that returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-main.c
+++ 0x14-bit_manipulation/5-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	unsigned int n;
 
-	n = flig_bits(1024, 1);
+	n = flip_bits(1024, 1);
 	printf("%u\n", n);
-	n = flig_bits(402, 98);
+	n = flip_bits(402, 98);
 	printf("%u\n", n);
-	n = flig_bits(1024, 3);
+	n = flip_bits(1024, 3);
 	printf("%u\n", n);
-	n = flig_bits(1024, 1025);
+	n = flip_bits(1024, 1025);
 	printf("%u\n", n);
 	return (0);
 }
