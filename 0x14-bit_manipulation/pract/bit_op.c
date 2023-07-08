3c826d9390c941f80fdc675f0a7b0df6444b1cd6
johnitopaisah <johnitopaisah@gmail.com>
2023-07-08 08:19:16 +0100

    practicing

--- 0x14-bit_manipulation/pract/bit_op.c
+++ 0x14-bit_manipulation/pract/bit_op.c
@@ -9,11 +9,14 @@ int main(void)
 {
 	int a = 10;
 	int b = 6;
+	int c, d;
 
+	c = a << 4;
+	d = a >> 2;
 	printf("%d\n", (a & b));
 	printf("%d\n", (a | b));
 	printf("%d\n", (a ^ b));
 	printf("%d\n", (a & b && b + 1 || 0 && b++));
-	printf("%d\n", b);
+	printf("b= %d, c= %d, d= %d\n", b, c, d);
 	return (0);
 }
