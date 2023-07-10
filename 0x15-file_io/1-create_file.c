919673ebc0170626b0aa8854922a1e845c421228
johnitopaisah <johnitopaisah@gmail.com>
2023-07-10 18:42:40 +0100

    function that creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		for (len = 0; text_content[len]; len++)
 			;
-	
-	opn = open(filename, 
+
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(opn, text_content, len);
+
+	if (opn == -1 || wrt == -1)
+		return (-1);
+	close(opn);
+
+	return (1);
 }
