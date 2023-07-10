3523af7f2b96ed7d305765248d5ab59c0147baaa
johnitopaisah <johnitopaisah@gmail.com>
2023-07-10 21:28:51 +0100

    updates the header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+/**************** All the function prototypes for task 3 ***************/
+char *create_buffer(char *fil);
+void close_file(int fd);
+int main(int ac, char *av[]);
+
 #endif /* MAIN_H */
