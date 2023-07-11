d57197bc08ff04a9ff491a2cfa9e54d67acd6381
johnitopaisah <johnitopaisah@gmail.com>
2023-07-11 13:10:22 +0100

    Advance

--- 0x15-file_io/100-elf_header_0.c
+++ 0x15-file_io/100-elf_header_0.c
@@ -112,3 +112,24 @@ void print_osabi(unsigned char *e_ident)
 			printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 	}
 }
+
+/**
+ * print_entry - Function that prints the entry point of an ELF header
+ * @e_entry: The address of the ELF entry point.
+ * @e_ident: pointer to an array contaning the ELF class
+ */
+void print_entry(unsigned int e_entry, unsigned char *e_ident)
+{
+	printf(" Entry point address:             ");
+
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	{
+		e_entry = (((e_entry << 8) & 0xFF00FF00) |
+			((e_entry >> 8) & 0xFF00FF00));
+		e_entry = ((e_entry << 16) | (e_entry >> 16));
+	}
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+		printf("%#x\n", (unsigned int)e_entry);
+	else
+		printf("%#x\n", e_entry);
+}
