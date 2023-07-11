752d5ebe6eb044704a005da79a9a34eda1336618
johnitopaisah <johnitopaisah@gmail.com>
2023-07-11 11:32:17 +0100

    updates the header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,4 +17,16 @@ char *create_buffer(char *fil);
 void close_file(int fd);
 int main(int ac, char *av[]);
 
+/**************** All the function prototypes for Advance ******************/
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
 #endif /* MAIN_H */
