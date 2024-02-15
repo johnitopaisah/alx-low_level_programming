01c3943f5bddc79eac4abe7f4d702a431c031368
John Itopa ISAH <johnitopaisah@MacBook-Pro-de-John.local>
2024-02-15 21:08:28 +0100

    task one

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,34 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+* struct hash_node_s - Node fo a hash table
+* @key: The key, string the unique in the HashTabls
+* @value: The value corresponding to a key
+* @next: A pointer to the next node of the List
+*/
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+* struct hash_table_s - Hash table data structure
+* @size: The size of trhe array
+* @array: An array of size at the size
+*/
+typedef struc hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+
+hash_table_t *hash_table_create(unsigned long int size);
